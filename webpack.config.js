6492d3cb8e37a81116fe94e39b52fcac0acf3c9d
chenyin <cyin@163.com>
2019-03-23 17:13:33 +0800

    设置打包的环境，有development和production两个值，production是生产环境，代码会被压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.exports = {
+    mode: 'development', //设置环境，有development和production两个值，production是生产环境，代码会被压缩
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
