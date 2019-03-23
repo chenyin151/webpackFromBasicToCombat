55b1e6621c1ebfb6ea78390422385088391208cb
chenyin <cyin@163.com>
2019-03-23 16:50:25 +0800

    我们运用webpack.config.js的script命令进行打包，这里注意： 1.若我们用这种方式打包，script命令就不需要用npx的方式运行打包命令，程序会自动在当前项目下去查找是否有webpack. 在这里直接用 "scripts": {     "bundle": "webpack"   }即可

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var Header = require('./header.js');
+var Content = require('./content.js');
+var Sidebar = require('./sidebar.js');
+
+new Header();
+new Content();
+new Sidebar();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'bundle')
+        path: path.resolve(__dirname, 'dist')
     }
 }
