d188f1f94c43c2f754255c7447802b3be99e5e7d
chenyin <cyin@163.com>
2019-03-26 00:10:58 +0800

    HMR对js的热更新，比如我们建立了2个js文件，实现点击一个DIV里面的数字会增加，当我们更改其中的一个js文件源码，只刷新它的结果而不刷新另一个js结果，这个时候我们就要用到HMR技术,跟CSS热更新的唯一区别就是要在调用方加入如下代码： if (module.hot) {     module.hot.accept('./number', ()=> {         document.body.removeChild(document.getElementById('div'))         new number();     }) } 这句的意思就是若开启了热更新功能的话，那么当监听到number代码有改动的时候先删除之前的元素，在把最新结果的元素加入到节点中

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import './style.css'
-var btn = document.createElement('button');
-btn.innerHTML = '新增';
-document.body.appendChild(btn);
-btn.onclick = function() {
-    var div = document.createElement('div');
-    div.innerHTML = 'item';
-    document.body.appendChild(div);
+import number from "./number";
+import counter from "./counter";
+
+new number();
+new counter()
+
+if (module.hot) {
+    module.hot.accept('./number', () => {
+        document.body.removeChild(document.getElementById('div'));
+        new number();
+    })
 }
---
+++ src/number.js
@@ -0,0 +1,7 @@
+function number() {
+    var div = document.createElement('div');
+    div.setAttribute('id','div')
+    div.innerHTML = 3000;
+    document.body.appendChild(div);
+}
+export default number
