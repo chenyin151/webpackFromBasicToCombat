fb38d1d110b3e586490e533fcdc82e415e2a1dad
chenyin <cyin@163.com>
2019-04-15 23:37:28 +0800

    Vue-cli3的配置方法2 在vue2版本我们在static目录下建立个文件，我们直接可以通过http访问到，但是在3.0版本中是不行的，但是我们也需要它能访问到该怎么办呢？这个时候我们就要用到devServer了，我们在vue.config.js中增加内容： module.exports = {     outputDir: 'dell',     configurationWebpack: {         devServer: {             contentBase: [path.resolve(__dirname, 'static')]         }     } } configuratureWebpack可以直接设置webpack原生配置，在这里进行了融合，这个时候它不仅会在我们打包的目录下找内容，还会到public和static下面找内容,这个时候我们就可以访问到static里面的内容了，其实我们最靠谱的方式是： module.exports = {     outputDir: 'dell',         devServer: {             contentBase: [path.resolve(__dirname, 'static')]     } } 这里可以看出来Vue已经直接提供了devServer方法，无需通过webpack配置来修改

--- Vue-cli3配置方法/my-project/vue.config.js
+++ Vue-cli3配置方法/my-project/vue.config.js
@@ -1,20 +1,7 @@
+const path = require('path');
 module.exports = {
     outputDir: 'dell',
-    pages: {
-        index: {
-            entry: 'src/index/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
-            title: 'Index Page',
-            chunks: ['chunk-vendors','chunk-common', 'index']
-        },
-        list: {
-            entry: 'src/index/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
-            title: 'Index Page',
-            chunks: ['chunk-vendors','chunk-common', 'index']
-        }
-        
+    devServer: {
+        contentBase: [path.resolve(__dirname, 'static')]
     }
 }
